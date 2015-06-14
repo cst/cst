24c9fd83c149d7733e0b6ae73069b55925a480e9
Marat Dulin <mdevils@gmail.com>
2015-06-14 19:24:48 +0200

    WithStatement :-/

---
+++ test/lib/nodes/types/WithStatement.js
@@ -0,0 +1,42 @@
+import {parseAndGetStatement} from '../../../utils';
+import {expect} from 'chai';
+
+describe('WithStatement', () => {
+    it('should return correct type', () => {
+        expect(parseAndGetStatement('with(true);').type).to.equal('WithStatement');
+    });
+
+    it('should accept single statement', () => {
+        var statement = parseAndGetStatement('with (true) x;');
+        expect(statement.object.type).to.equal('Literal');
+        expect(statement.object.value).to.equal(true);
+        expect(statement.body.type).to.equal('ExpressionStatement');
+        expect(statement.body.expression.type).to.equal('Identifier');
+        expect(statement.body.expression.name).to.equal('x');
+    });
+
+    it('should accept expression in parentheses', () => {
+        var statement = parseAndGetStatement('with ((true)) x;');
+        expect(statement.object.type).to.equal('Literal');
+        expect(statement.object.value).to.equal(true);
+        expect(statement.body.type).to.equal('ExpressionStatement');
+        expect(statement.body.expression.type).to.equal('Identifier');
+        expect(statement.body.expression.name).to.equal('x');
+    });
+
+    it('should accept whitespaces', () => {
+        var statement = parseAndGetStatement('with ( true ) x ;');
+        expect(statement.object.type).to.equal('Literal');
+        expect(statement.object.value).to.equal(true);
+        expect(statement.body.type).to.equal('ExpressionStatement');
+        expect(statement.body.expression.type).to.equal('Identifier');
+        expect(statement.body.expression.name).to.equal('x');
+    });
+
+    it('should accept blocks', () => {
+        var statement = parseAndGetStatement('with ( true ) { x; }');
+        expect(statement.object.type).to.equal('Literal');
+        expect(statement.object.value).to.equal(true);
+        expect(statement.body.type).to.equal('BlockStatement');
+    });
+});
