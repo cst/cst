120009f5ccb56e93ae4e98e42bc44ef05cf37cc3
Marat Dulin <mdevils@gmail.com>
2015-06-14 11:06:55 +0200

    Fixes for Literal, better tests

--- test/lib/nodes/types/Literal.js
+++ test/lib/nodes/types/Literal.js
@@ -1,9 +1,38 @@
 import {parseAndGetExpression} from '../../../utils';
 import {expect} from 'chai';
 
-describe('LabeledStatement', () => {
+describe('Literal', () => {
     it('should return correct type', () => {
         expect(parseAndGetExpression('1').type).to.equal('Literal');
+    });
+
+    it('should accept double quoted string', () => {
+        var expression = parseAndGetExpression('"123"');
+        expect(expression.type).to.equal('Literal');
+        expect(expression.value).to.equal('123');
+        expect(expression.raw).to.equal('"123"');
+    });
+
+    it('should accept single quoted string', () => {
+        var expression = parseAndGetExpression('\'123\'');
+        expect(expression.type).to.equal('Literal');
+        expect(expression.value).to.equal('123');
+        expect(expression.raw).to.equal('\'123\'');
+    });
+
+    it('should accept single quoted string', () => {
+        var expression = parseAndGetExpression('\'123\'');
+        expect(expression.type).to.equal('Literal');
+        expect(expression.value).to.equal('123');
+        expect(expression.raw).to.equal('\'123\'');
+    });
+
+    it('should accept string escapes', () => {
+        var value = '" \\" \\n \\r \\t \\f \\b \\v \\0 \\\n \\u006F \\251 \\xa9 \\u{000000000061} "';
+        var expression = parseAndGetExpression(value);
+        expect(expression.type).to.equal('Literal');
+        expect(expression.value).to.equal(' " \n \r \t \f \b \u000b \0  o © © a ');
+        expect(expression.raw).to.equal(value);
     });
 
     it('should accept integer', () => {
