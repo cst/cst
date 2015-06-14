6e8976bfedfb1cb0f5bea296ec157fe9ae145e5b
Marat Dulin <mdevils@gmail.com>
2015-06-14 20:07:05 +0200

    TryStatement, CatchClause

---
+++ test/lib/nodes/types/TryStatement.js
@@ -0,0 +1,22 @@
+import {parseAndGetStatement} from '../../../utils';
+import {expect} from 'chai';
+
+describe('TryStatement', () => {
+    it('should return correct type', () => {
+        expect(parseAndGetStatement('try{;}finally{;}').type).to.equal('TryStatement');
+    });
+
+    it('should accept statements', () => {
+        var statement = parseAndGetStatement('try { x; } finally { y; }');
+        expect(statement.block.body[0].expression.name).to.equal('x');
+        expect(statement.finalizer.body[0].expression.name).to.equal('y');
+    });
+
+    it('should accept catch statement', () => {
+        var statement = parseAndGetStatement('try { x; } catch ( e ) { y; } finally { z; }');
+        expect(statement.block.body[0].expression.name).to.equal('x');
+        expect(statement.handler.param.name).to.equal('e');
+        expect(statement.handler.body.body[0].expression.name).to.equal('y');
+        expect(statement.finalizer.body[0].expression.name).to.equal('z');
+    });
+});
