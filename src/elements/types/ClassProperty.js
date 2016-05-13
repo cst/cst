3ae7bb786f251f39246c91166f2acd0290610eee
Oleg Gaidarenko <markelog@gmail.com>
2016-05-13 22:03:47 +0300

    Add support for `ClassProperty`
    Fixes #112

---
+++ src/elements/types/ClassProperty.js
@@ -0,0 +1,32 @@
+import Node from '../Node';
+
+export default class ClassProperty extends Node {
+    constructor(childNodes) {
+        super('ClassProperty', childNodes);
+    }
+
+    _acceptChildren(children) {
+        this.static = false;
+
+        if (children.isToken('Identifier', 'static')) {
+            this.static = true;
+            children.passToken();
+            children.skipNonCode();
+        }
+
+        this.key = children.passAssignable();
+        children.skipNonCode();
+
+        children.passToken('Punctuator', '=');
+        children.skipNonCode();
+
+        this.value = children.passNode();
+
+        children.assertEnd();
+
+        // What is computed Class property?
+        // See https://github.com/jeffmo/es-class-fields-and-static-properties/issues/33
+        // It seems babylon doesn't support it at this stage
+        this.computed = false;
+    }
+}
---
+++ test/lib/elements/types/ClassProperty.js
@@ -0,0 +1,20 @@
+import {expect} from 'chai';
+
+import {parseAndGetClassMember} from '../../../utils';
+
+describe('ClassProperty', () => {
+    it('accepts property', () => {
+        let member = parseAndGetClassMember('x = 1');
+        expect(member.key.type).to.equal('Identifier');
+        expect(member.key.name).to.equal('x');
+        expect(member.value.type).to.equal('NumericLiteral');
+        expect(member.value.value).to.equal(1);
+        expect(member.static).to.equal(false);
+        expect(member.computed).to.equal(false);
+    });
+
+    it('accepts static property', () => {
+        let member = parseAndGetClassMember('static x = 1');
+        expect(member.static).to.equal(true);
+    });
+});
