c82f895c17ff742c211d1555088485e040a16de1
Thomas Grainger <tom@procensus.com>
2017-06-01 18:18:40 +0100

    use ClassBody from master

--- src/elements/types/ClassBody.js
+++ src/elements/types/ClassBody.js
@@ -1,10 +1,5 @@
 import Node from '../Node';
 
-function isCode({currentElement}) {
-    const {isCode} = currentElement || {};
-    return (isCode !== false);
-}
-
 export default class ClassBody extends Node {
     constructor(childNodes) {
         super('ClassBody', childNodes);
@@ -18,19 +13,20 @@ export default class ClassBody extends Node {
         children.skipNonCode();
 
         while (!children.isToken('Punctuator', '}')) {
-            if (children.isToken('Punctuator', ';')) {
-                // For the class Test { x() {}; } case
-                children.passToken('Punctuator', ';');
-            } else if (!isCode(children)) {
-                children.skipNonCode();
-            } else {
-                children.assertOneOfNode(['ClassProperty', 'ClassMethod']);
-                if (children.isNode('ClassProperty')) {
-                    body.push(children.passNode('ClassProperty'));
-                } else if (children.isNode('ClassMethod')) {
-                    body.push(children.passNode('ClassMethod'));
-                }
+            if (children.isNode('ClassProperty')) {
+                body.push(children.passNode('ClassProperty'));
             }
+
+            if (children.isNode('ClassMethod')) {
+                body.push(children.passNode('ClassMethod'));
+            }
+
+            // For the class Test { x() {}; } case
+            if (children.isToken('Punctuator', ';')) {
+                children.passToken('Punctuator', ';');
+            }
+
+            children.skipNonCode();
         }
 
         children.passToken('Punctuator', '}');
